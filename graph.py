46be7be39529e5280c9c9c920f78acca70bc93ac
Louis Leduc <leduclouis64@hotmail.fr>
2018-12-19 12:25:55 +0100

    correction du bug de multiplication de dictionnaire

--- graph.py
+++ graph.py
@@ -26,26 +26,27 @@ class Graph:
             theta_step = problem.getThetaStep()
             robot_radius = problem.getRobotRadius()
 
+            #toutes les positions de defenseurs
             array_width = numpy.arange(-(field_width/2) + pos_step, field_width, pos_step)
             array_height = numpy.arange((int(field_height/2)) + pos_step, -field_height, -pos_step)
 
             for i in array_width:
                 for j in array_height:
-                    defender = {}
-                    defender[0] = i
-                    defender[1] = j
+                    defender = [i, j]
                     theta_step_tmp = theta_step
                     for o in range (0, problem.getNbOpponents(), 1):
-                        while theta_step_tmp < 1: ############## test pour le while, pas du tout sure de la condition
+                        #while theta_step_tmp < 1: ############## test pour le while, pas du tout sure de la condition
                             #print (problem.field_limits)
                             #kick = segmentLineIntersection(problem.getOpponent(o), problem.field_limits[0][0],problemproblem.field_limits[0][1])
                             #if kick is not None:
                             #i = 0
                             #for j in range (0,5,1)
-                            if segmentCircleIntersection(problem.getOpponent(o),problem.field_limits[0], defender, robot_radius) is not None:
-                                with open('graph.json', 'w', encoding='utf-8') as solution_file:
-                                    data["defenders"] = defender
-                            theta_step_tmp += theta_step
+                        var = segmentCircleIntersection(problem.getOpponent(o),problem.field_limits[0], defender, robot_radius)
+                        print(var)
+                        if segmentCircleIntersection(problem.getOpponent(o),problem.field_limits[0], defender, robot_radius) is not None:
+                            with open('graph.json', 'w', encoding='utf-8') as solution_file:
+                                data["defenders"] = defender
+                        #theta_step_tmp += theta_step
 
 
     def addNode(opponent, defender):
